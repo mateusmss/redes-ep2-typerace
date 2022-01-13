4c1221c46c60664837d1fde439e21683106e93b8
Mateus Santos Saldanha <Mateusaldanha@usp.br>
2022-01-13 03:37:54 -0300

    mais gets e método comparação

--- server/src/main/java/br/usp/each/typerace/server/Player.java.java
+++ server/src/main/java/br/usp/each/typerace/server/Player.java.java
@@ -1,17 +1,18 @@
 
 package br.usp.each.typerace.server;
-
+import java.util.Set;
 public class java {
-    private Set<String> 
+    private Set<String> palavras;
     private String playerID;
     private int correto,errado;
     
-    public Player(String playerID, int errado, int correto){
+    public Player(String playerID, int errado, int correto, Set<String>palavras){
         this.playerID = playerID;
         this.errado = errado;
         this.correto = correto;
+        this.palavras = palavras;
     }
-    
+    //Geters e seters
     public String getPlayerID(){
         return this.playerID;
     }
@@ -27,5 +28,17 @@ public class java {
     public int getRespostaErrada(){
         return this.errado;
     }
+    public Set<String> getPalavras(){
+        return this.palavras;
+    }
+    //Métodos de comparação
+    
+    /*public void compararPalavras(String entrada, String respostaCorreta){
+        if(correto.equals(entrada)){
+            Set.add(entrada);
+        }else{
+            setRespostaErrada();
+        }
+    }*/
     
 }
