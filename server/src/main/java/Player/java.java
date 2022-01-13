ca1544f7d360f86f1948659f665996e6245290ec
Mateus Santos Saldanha <Mateusaldanha@usp.br>
2022-01-13 01:44:02 -0300

    Criação classe Player

---
+++ server/src/main/java/Player/java.java
@@ -0,0 +1,15 @@
+
+package Player;
+
+public class java {
+    private Set<String> 
+    private String playerID;
+    private int correto,errado;
+    
+    public Player(String playerID, int errado, int correto){
+        this.playerID = playerID;
+        this.errado = errado;
+        this.correto = correto;
+        
+    }
+}
