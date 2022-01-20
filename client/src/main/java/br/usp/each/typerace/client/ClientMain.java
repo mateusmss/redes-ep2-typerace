1243ded59dc0c81331ef2dd6acbe484225fa1ac6
Mateus Santos Saldanha <Mateusaldanha@usp.br>
2022-01-20 04:38:46 -0300

    Ajustes na entrada de dados e verificação de duplicidade

--- client/src/main/java/br/usp/each/typerace/client/ClientMain.java
+++ client/src/main/java/br/usp/each/typerace/client/ClientMain.java
@@ -4,6 +4,7 @@ import org.java_websocket.client.WebSocketClient;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Scanner;
 
 public class ClientMain {
 
@@ -19,7 +20,7 @@ public class ClientMain {
     }
 
     public static void main(String[] args) {
-        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+        Scanner ler = new Scanner(System.in);
         WebSocketClient client;
 
         System.out.println("Servidor localhost:8080 ");
@@ -28,9 +29,10 @@ public class ClientMain {
         if (server.isEmpty())
             server = "ws://localhost:8080";
         boolean condição = true;
+        
         while(condição) {
-            System.out.println("Digite seu nome de usuárioo: ");
-            String nome = input.readLine();
+            System.out.println("Digite seu nome de usuário: ");
+            String nome = ler.nextLine();
             String conexao = server;
 
             if (nome.isEmpty()) {
@@ -43,9 +45,12 @@ public class ClientMain {
             client = new Client(new URI(conexao));
             ClientMain main = new ClientMain(client);
 
-            main.init(nome);
-
-    }
+            main.init(nome);    
+        }
+        while(true) {
+            String tentativa = ler.nextLine();
+            client.send(tentativa);
+        }
     
     
 }
