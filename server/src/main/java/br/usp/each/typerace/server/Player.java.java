6a8f2addb0764e9f8c35fea3c18ecaa4686a1d39
Mateus Santos Saldanha <Mateusaldanha@usp.br>
2022-01-20 03:29:07 -0300

    modificações na classe Player

--- server/src/main/java/br/usp/each/typerace/server/Player.java.java
+++ server/src/main/java/br/usp/each/typerace/server/Player.java.java
@@ -2,15 +2,15 @@
 package br.usp.each.typerace.server;
 import java.util.Set;
 public class java {
-    private int listaPalavras;
+    private Set<String> palavras;
     private String playerID;
     private int correto,errado;
     
-    public Player(String playerID, int errado, int correto){
+    public Player(String playerID, int errado, int correto, Set<String> listaPalavras){
         this.playerID = playerID;
         this.errado = errado;
         this.correto = correto;
-        this.listaPalavras = 0;
+        this.palavras = listaPalavras;
     }
     //Geters e seters
     public String getPlayerID(){
@@ -28,23 +28,64 @@ public class java {
     public int getRespostaErrada(){
         return this.errado;
     }
-    public int getPalavras(){
+    public Set<String> getPalavras() {
         return this.listaPalavras;
     }
-    public void incrementarListaPalavras(){
-        this.listaPalavras++;
-    }
-    
-    //Métodos de comparação
-    
-    //verificar se a resposta está correta ou errada
-    public boolean compararPalavras(String entrada, String respostaCorreta){
-        if(correto.equals(entrada)){
+
+    public boolean checkAnswer(String palavraResposta) {
+
+        if(getPalavras().contains(palavraResposta.toUpperCase())){
             setRespostaCorreta();
+            getPalavras().remove(palavraResposta.toUpperCase());
+            return true;
         }else{
             setRespostaErrada();
         }
-        incrementarListaPalavras();
+        return false;
     }
+
+    public boolean checkAnswer(String resposta) {
+
+        if(getPalavras().contains(resposta.toUpperCase())){
+            setRespostaCorreta();
+            getPalavras().remove(resposta.toUpperCase());
+            return true;
+        }else{
+            setRespostaErrada();
+            return false;
+        }
+    }
+    public int compareTo(Player x) {
+        if(this.correto != x.correto){
+            return this.correto - x.correto;
+        }else{
+            return x.errado - this.errado;
+    }
+        
+    @Override
+    public boolean equals(Object o) {
+        if (this == o){
+            return true;
+        } 
+        if (o == null || getClass() != o.getClass()){
+            return false;
+        } 
+        
+        Player player = (Player) o;
+        return playerId.equals(player.playerId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(playerId, correto, errado);
+    }
+    
+    public String toString() {
+        return this.playerId + " - " + this.correto + " - " + this.errado;
+    }
+
+        
+    }
+    
     
 }
